7916b91b8fd7da43e1be5dd6609caf2b2f753314
Margie Roswell <mroswell@gmail.com>
2017-03-31 20:03:11 -0400

    build components

---
+++ app/index.js
@@ -0,0 +1,84 @@
+const person = {
+  name: 'Chris Castig',
+  location: 'Brooklyn, New York',
+  occupation: {
+    title: 'Protecting Freedom',
+    employer: '@onemonthedu'
+  },
+  photo: 'https://unsplash.it/g/300/200',
+  updates: [
+    {
+      platform: 'twitter',
+      status: 'I\'m happy, hope you\'re happy too!'
+    },
+    {
+      platform: 'twitter',
+      status: 'The better the singer\'s voice, the harder it is to hear what they\'re saying'
+    },
+    {
+      platform: 'twitter',
+      status: 'Fear makes the wolf look bigger'
+    },
+    {
+      platform: 'facebook',
+      status: 'If you\'re working on something that you think is going to get accomplished in this lifetime then you\'re not thinking big enough'
+    }
+  ]
+};
+
+class Photo extends React.Component {
+
+  render() {
+
+    return (
+      <div className="photo">
+      <img src="https://unsplash.it/g/300/200
+    " alt="Photo" />
+    </div>
+  )
+  }
+}
+
+class Bio extends React.Component {
+
+  render() {
+
+    return (
+      <div className="bio">
+      <h1 className="name">{this.props.name}</h1>
+      <h2 className="location">{this.props.location}</h2>
+      <div className="occupation">
+      <p>{this.props.occupation.title} at {this.props.occupation.employer}</p>
+    </div>
+    </div>
+  )
+  }
+}
+
+class Updates extends React.Component {
+  render() {
+    return (
+      <div className="updates">
+      <ul>
+      <li className="update">Updates</li>
+      <li className="update">Updates</li>
+      </ul>
+      </div>
+  )
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    return(
+      <div className="card">
+      <Photo photo={person.photo} />
+      <Bio name ={person.name} location={person.location} occupation = {person.occupation}/>
+      <Updates />
+      </div>
+  )
+  }
+}
+
+ReactDOM.render(<Card />,document.getElementById('app')
+);
---
+++ build/index.js
@@ -0,0 +1,103 @@
+const person = {
+  name: 'Chris Castig',
+  location: 'Brooklyn, New York',
+  occupation: {
+    title: 'Protecting Freedom',
+    employer: '@onemonthedu'
+  },
+  photo: 'https://unsplash.it/g/300/200',
+  updates: [{
+    platform: 'twitter',
+    status: 'I\'m happy, hope you\'re happy too!'
+  }, {
+    platform: 'twitter',
+    status: 'The better the singer\'s voice, the harder it is to hear what they\'re saying'
+  }, {
+    platform: 'twitter',
+    status: 'Fear makes the wolf look bigger'
+  }, {
+    platform: 'facebook',
+    status: 'If you\'re working on something that you think is going to get accomplished in this lifetime then you\'re not thinking big enough'
+  }]
+};
+
+class Photo extends React.Component {
+
+  render() {
+
+    return React.createElement(
+      'div',
+      { className: 'photo' },
+      React.createElement('img', { src: 'https://unsplash.it/g/300/200 ', alt: 'Photo' })
+    );
+  }
+}
+
+class Bio extends React.Component {
+
+  render() {
+
+    return React.createElement(
+      'div',
+      { className: 'bio' },
+      React.createElement(
+        'h1',
+        { className: 'name' },
+        this.props.name
+      ),
+      React.createElement(
+        'h2',
+        { className: 'location' },
+        this.props.location
+      ),
+      React.createElement(
+        'div',
+        { className: 'occupation' },
+        React.createElement(
+          'p',
+          null,
+          this.props.occupation.title,
+          ' at ',
+          this.props.occupation.employer
+        )
+      )
+    );
+  }
+}
+
+class Updates extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      { className: 'updates' },
+      React.createElement(
+        'ul',
+        null,
+        React.createElement(
+          'li',
+          { className: 'update' },
+          'Updates'
+        ),
+        React.createElement(
+          'li',
+          { className: 'update' },
+          'Updates'
+        )
+      )
+    );
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      { className: 'card' },
+      React.createElement(Photo, { photo: person.photo }),
+      React.createElement(Bio, { name: person.name, location: person.location, occupation: person.occupation }),
+      React.createElement(Updates, null)
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(Card, null), document.getElementById('app'));
